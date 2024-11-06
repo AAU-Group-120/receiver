f70b5447da8f07c5b7ade2433aecc0b1b6d027b7
SquaxLife <maxkunckel@protonmail.com>
2024-11-06 11:29:01 +0100

    Configuration and setup of MCU
    Fixed setup and configuration for the MCU and wrote basic text example to test it.

--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,34 @@
 #include <SPI.h>
 
 // put function declarations here:
-int myFunction(int, int);
+
+TFT_eSPI tft = TFT_eSPI();           // TFT object
+#define BGCOLOR 0x0000
+#define TXTCOLOR 0xFFFF
+#define TXTSIZE 2
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+ 
+  tft.init();
+
+  Serial.begin(115200); // Debug only
+
+  tft.begin();  // initialize
+  tft.setRotation(3);
+  tft.fillScreen(BGCOLOR);
+  tft.setTextColor(TXTCOLOR);
+  tft.setTextSize(TXTSIZE);
+  tft.setCursor(0,0,2);
+  tft.print("Inductor");
+
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+
+
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
