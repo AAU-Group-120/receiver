8f97ee76ea49ee09837e36234d5ff89dbe3b5348
SquaxLife <maxkunckel@protonmail.com>
2024-11-11 12:03:17 +0100

    Bitmask feature
    Added a bitmask feature for detecting different error messages received in a byte format.

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,16 @@ TFT_eSPI tft = TFT_eSPI();           // TFT object
 #define BGCOLOR 0x0000
 #define TXTCOLOR 0xFFFF
 #define TXTSIZE 2
-String Errormessage = "Højre blinklys";
+#define B0 B0
+#define B1 B1
+char Testbyte = 16 ;
+int B0, B1, B2, B3, B4, B5, B6, B7, RESULT;
+String Errormessage1 = "Højre Blinklys";
+String Errormessage2 = "Venstre Blinklys";
+String Errormessage3 = "Højre Kørelys";
+String Errormessage4 = "Venstre Kørelys";
+String Errormessage5 = "Stop Lys";
+String Errormessage6 = "Tåge Lys";
 
 void setup() {
   // put your setup code here, to run once:
@@ -18,13 +27,52 @@ void setup() {
   Serial.begin(115200); // Debug only
 
   tft.begin();  // initialize
-  tft.setRotation(3);
+  tft.setRotation(2);
   tft.fillScreen(BGCOLOR);
-  tft.setTextColor(TXTCOLOR);
+  tft.setTextColor(TFT_RED);
   tft.setTextSize(TXTSIZE);
   tft.setCursor(0,0,0);
-  tft.print("Fejl: ");
-  tft.println(Errormessage);
+
+  
+     B0 = (Testbyte & 0x01) ? 1:0;
+     B1 = (Testbyte & 0x02) ? 1:0;
+     B2 = (Testbyte & 0x04) ? 1:0;
+     B3 = (Testbyte & 0x08) ? 1:0;
+     B4 = (Testbyte & 0x10) ? 1:0;
+     B5 = (Testbyte & 0x20) ? 1:0;
+     B6 = (Testbyte & 0x40) ? 1:0;
+     B7 = (Testbyte & 0x80) ? 1:0;
+  
+
+RESULT = B0+B1+B2+B3+B4+B5+B6+B7;
+
+if (RESULT==1)
+{
+if (B0==1){
+  tft.println(Errormessage1);
+}
+else if (B1==1){
+  tft.println(Errormessage2);
+}
+else if (B2==1){
+  tft.println(Errormessage3);
+}
+else if (B3==1){
+  tft.println(Errormessage4);
+}
+else if (B4==1){
+  tft.println(Errormessage5);
+}
+else if (B5==1){
+  tft.println(Errormessage6);
+}
+else if (B6==1){
+  tft.println("1");
+}
+else {
+  tft.println("2");
+}
+}
 
 
 }
@@ -32,10 +80,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-tft.fillTriangle(70, 110, 120, 35, 170, 110, TFT_YELLOW);
-  delay(1000);
-tft.fillTriangle(70, 110, 120, 35, 170, 110, TFT_BLACK);
-  delay(1000);
+
 
 }
 
