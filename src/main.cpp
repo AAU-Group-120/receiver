ae4baf0ea3869f2a1f6a52a4a6e0bebf28205f47
SquaxLife <maxkunckel@protonmail.com>
2024-11-11 12:39:33 +0100

    Minor changes
    Minor changes in code for bitmask.

--- src/main.cpp
+++ src/main.cpp
@@ -8,16 +8,16 @@ TFT_eSPI tft = TFT_eSPI();           // TFT object
 #define BGCOLOR 0x0000
 #define TXTCOLOR 0xFFFF
 #define TXTSIZE 2
-#define B0 B0
-#define B1 B1
 char Testbyte = 16 ;
-int B0, B1, B2, B3, B4, B5, B6, B7, RESULT;
+int b0, b1, b2, b3, b4, b5, b6, b7, RESULT;
 String Errormessage1 = "Højre Blinklys";
 String Errormessage2 = "Venstre Blinklys";
 String Errormessage3 = "Højre Kørelys";
 String Errormessage4 = "Venstre Kørelys";
 String Errormessage5 = "Stop Lys";
 String Errormessage6 = "Tåge Lys";
+
+void printError ();
 
 void setup() {
   // put your setup code here, to run once:
@@ -34,44 +34,21 @@ void setup() {
   tft.setCursor(0,0,0);
 
   
-     B0 = (Testbyte & 0x01) ? 1:0;
-     B1 = (Testbyte & 0x02) ? 1:0;
-     B2 = (Testbyte & 0x04) ? 1:0;
-     B3 = (Testbyte & 0x08) ? 1:0;
-     B4 = (Testbyte & 0x10) ? 1:0;
-     B5 = (Testbyte & 0x20) ? 1:0;
-     B6 = (Testbyte & 0x40) ? 1:0;
-     B7 = (Testbyte & 0x80) ? 1:0;
+     b0 = (Testbyte & 0x01) ? 1:0;
+     b1 = (Testbyte & 0x02) ? 1:0;
+     b2 = (Testbyte & 0x04) ? 1:0;
+     b3 = (Testbyte & 0x08) ? 1:0;
+     b4 = (Testbyte & 0x10) ? 1:0;
+     b5 = (Testbyte & 0x20) ? 1:0;
+     b6 = (Testbyte & 0x40) ? 1:0;
+     b7 = (Testbyte & 0x80) ? 1:0;
   
 
-RESULT = B0+B1+B2+B3+B4+B5+B6+B7;
+RESULT = b0+b1+b2+b3+b4+b5+b6+b7;
 
 if (RESULT==1)
 {
-if (B0==1){
-  tft.println(Errormessage1);
-}
-else if (B1==1){
-  tft.println(Errormessage2);
-}
-else if (B2==1){
-  tft.println(Errormessage3);
-}
-else if (B3==1){
-  tft.println(Errormessage4);
-}
-else if (B4==1){
-  tft.println(Errormessage5);
-}
-else if (B5==1){
-  tft.println(Errormessage6);
-}
-else if (B6==1){
-  tft.println("1");
-}
-else {
-  tft.println("2");
-}
+printError();
 }
 
 
@@ -82,5 +59,33 @@ void loop() {
 
 
 
+}
+
+void printError()
+{
+if (b0==1){
+  tft.println(Errormessage1);
+}
+else if (b1==1){
+  tft.println(Errormessage2);
+}
+else if (b2==1){
+  tft.println(Errormessage3);
+}
+else if (b3==1){
+  tft.println(Errormessage4);
+}
+else if (b4==1){
+  tft.println(Errormessage5);
+}
+else if (b5==1){
+  tft.println(Errormessage6);
+}
+else if (b6==1){
+  tft.println("1");
+}
+else {
+  tft.println("2");
+}
 }
 
